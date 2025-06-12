58665833eaf12788b9d87e8ca74eb16fed1858a4
Travis Brown <travisrobertbrown@gmail.com>
2025-06-12 07:43:00 +0200

    Add experimentation project

---
+++ hacks/src/main.rs
@@ -0,0 +1,111 @@
+use archivindex::{
+    cdx::{item::ItemList, mime_type::MimeType},
+    surt::Surt,
+};
+use cli_helpers::prelude::*;
+use std::path::{Path, PathBuf};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let opts: Opts = Opts::parse();
+    opts.verbose.init_logging()?;
+
+    match opts.command {
+        Command::CheckSurts { input } => {
+            let cdx_paths = find_cdx_files(input)?;
+            let mut success_count = 0;
+            let mut failure_count = 0;
+
+            for path in cdx_paths {
+                let contents = std::fs::read_to_string(&path)?;
+
+                match serde_json::from_str::<ItemList>(&contents) {
+                    Ok(items) => {
+                        for item in items.values {
+                            if item.mime_type == MimeType::ApplicationJson {
+                                let converted_surt = Surt::from_url(&item.original)?;
+
+                                if converted_surt == item.key {
+                                    success_count += 1;
+                                } else {
+                                    log::error!(
+                                        "Invalid conversion in {:?}:\nConverted: {}\nOriginal:  {}",
+                                        path,
+                                        converted_surt,
+                                        item.key
+                                    );
+
+                                    failure_count += 1;
+                                }
+                            }
+                        }
+                    }
+                    Err(error) => {
+                        log::error!("At {:?}: {:?}", path, error);
+                    }
+                }
+            }
+
+            log::info!("Good: {}; bad: {}", success_count, failure_count);
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("I/O error")]
+    Io(#[from] std::io::Error),
+    #[error("CLI argument reading error")]
+    Args(#[from] cli_helpers::Error),
+    #[error("CSV error")]
+    Csv(#[from] csv::Error),
+    #[error("JSON error")]
+    Json(#[from] serde_json::Error),
+    #[error("SURT error")]
+    Surt(#[from] archivindex::surt::Error),
+}
+
+#[derive(Debug, Parser)]
+#[clap(name = "archivindex-hacks", version, author)]
+struct Opts {
+    #[clap(flatten)]
+    verbose: Verbosity,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Parser)]
+enum Command {
+    CheckSurts {
+        #[clap(long)]
+        input: PathBuf,
+    },
+}
+
+fn find_cdx_files<P: AsRef<Path>>(root: P) -> Result<Vec<PathBuf>, Error> {
+    let mut cdx_paths = std::fs::read_dir(root)?
+        .flat_map(|collection_entry| {
+            collection_entry
+                .and_then(|entry| std::fs::read_dir(entry.path()))
+                .map_or_else(|error| vec![Err(error)], |paths| paths.collect())
+        })
+        .flat_map(|screen_name_entry| {
+            screen_name_entry
+                .and_then(|entry| std::fs::read_dir(entry.path().join("data")))
+                .map_or_else(|error| vec![Err(error)], |paths| paths.collect())
+        })
+        .map(|entry| {
+            entry.and_then(|entry| {
+                let modified = entry.metadata()?.modified()?;
+
+                Ok((modified, entry.path()))
+            })
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    cdx_paths.sort_by_key(|(timestamp, _)| std::cmp::Reverse(*timestamp));
+
+    Ok(cdx_paths.into_iter().map(|(_, path)| path).collect())
+}
